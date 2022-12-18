d57e224fc25fbf25fe0ab97885f6e5b9f3cecc53
TroyFoote <troyfoote@gmail.com>
2022-12-18 15:04:20 +0800

    update pybank script

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,19 @@
-#import os
+# Pybank Script
+
+# #import os module
 import os
 
-#import csv
+#import csv module
 import csv
+
+#variables
+total = 0
+month_count = 0
+profit_loss_net = 0
+profit_loss_change = []
+ave_profit_loss_change = 0
+greatest_increase_profits = 0
+greatest_decrease_profits = 0
 
 #import csv file from computer
 budget_data_csv = os.path.join("Resources", "budget_data.csv")
@@ -12,7 +23,19 @@ with open(budget_data_csv) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
 
     csv_header = next(csv_file)
-    print(f"header: {csv_header}")
+    #print(f"Header: {csv_header}")
+
+    for row in csv_reader:
+        month_count +=1
+        total += int(row[1])
+
+    print(month_count)
+
+
+
+
+
+
 
     
 
