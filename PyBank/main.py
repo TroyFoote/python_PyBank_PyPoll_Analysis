df1d60ce4bc29273a5b092eef57bd8313e51643d
TroyFoote <troyfoote@gmail.com>
2022-12-24 07:01:04 +0800

    updated file

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,11 +11,11 @@ month_count = 0 #total months
 current_month_profit_loss = 0 
 total_profit_loss = 0 #all months added together
 previous_month_profit_loss = 0
-profit_loss_change = 0 #total of differences between months profit and loss
-ave_profit_loss_change = 0
+total_profit_loss_change = 0 #total of differences between months profit and loss
 greatest_increase_profits = 0
 greatest_decrease_profits = 0
-
+change_count = 0
+first_row = True
 
 #import csv file from computer
 budget_data_csv = os.path.join("Resources", "budget_data.csv")
@@ -24,27 +24,33 @@ budget_data_csv = os.path.join("Resources", "budget_data.csv")
 with open(budget_data_csv) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
 
-    csv_header = next(csv_file)
+    csv_header = next(csv_reader)
     
     #loop through csv
     for row in csv_reader:
         month_count +=1
-
         current_month_profit_loss = int(row[1])
         total_profit_loss += current_month_profit_loss
-          
+
+        if first_row:    
+            first_row = False   
+        else: 
+            change_count +=1
+            current_profit_loss_change = current_month_profit_loss - previous_month_profit_loss
+            total_profit_loss_change += current_profit_loss_change
+        previous_month_profit_loss = current_month_profit_loss
+
+    ave_profit_loss_change = round(total_profit_loss_change / change_count, 2)
+
     #total months & profit loss totals included in report
     print(month_count)
     print(total_profit_loss)
-
-    #changes in Profit/Loss per month over the full period
-    for row in csv_reader:
-        previous_month_profit_loss = int(row[1])
-        profit_loss_change = current_month_profit_loss - previous_month_profit_loss
-  
+     
     
-    print(profit_loss_change)
+    print(total_profit_loss_change)
     print(current_month_profit_loss)
+    print(change_count)
+    print(ave_profit_loss_change)
     
     
 
