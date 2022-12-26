cc9c7076168b2417d708e013ed09dfbed860f671
TroyFoote <troyfoote@gmail.com>
2022-12-26 16:58:25 +0800

    updated pyPoll file and created Analysis file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,6 @@
-# Pypoll Script
+#Pypoll Script
 
-# #import os module
+#import os module
 import os
 
 #import csv module
@@ -9,31 +9,11 @@ import csv
 #Variables
 total_votes = 0 #total votes cast
 candidate_list = []
-percent_votes_candidate = 0
-candidate_votes = 0
-winner = []
-candidate_list = ["Charles Casper Stockham","Diana DeGette","Raymon Anthony Doane" ]
-candidate1 = {
-  "name" : "Charles Casper Stockham",
-  "votes" : 0
-  }
-candidate2 = {
-  "name" :"Diana DeGette",
-  "votes" : 0
-}
-candidate3 = {
-  "name" : "Raymon Anthony Doane",
-  "votes" : 0
-}
-candidates ={
-    "candidate1" : candidate1,
-    "candidate2" : candidate2,
-    "candidate3" : candidate3
-}
-print(candidates)
-
-candidate_list = candidates
-print(candidate_list)
+#percent_votes_candidate = 0
+candidate_votes = {}
+winner = ""
+win_votes = 0
+win_percent = 0
 
 #import csv file from computer
 election_data_csv = os.path.join("Resources", "election_data.csv")
@@ -41,20 +21,73 @@ election_data_csv = os.path.join("Resources", "election_data.csv")
 #open csv file
 with open(election_data_csv) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
-
     csv_header = next(csv_file)
+    
 
-#loop through csv
+    #loop through csv
     for row in csv_reader:
         total_votes +=1
+        candidate_name = row[2]
+
+        #create list of candidates and votes per candidate
+        if candidate_name not in candidate_list:
+            candidate_list.append(candidate_name)
+            candidate_votes[candidate_name]= 0
+        candidate_votes[candidate_name]+=1
         
-        #candidate_votes = (row[2])
-      
+    #print election results        
+    print(f"Election Results")    
+    print(f"_________________________")
+    print(f"Total Votes: {total_votes}") 
+    print(f"_________________________")
+
+    #percentage of votes per candidate
+    for name in candidate_votes:
+        votes = candidate_votes[name]
+        percentage = votes / total_votes
+        percent = round(percentage *100 ,3)
+
+        #print results
+        print(f"{name} : {percent}%, {votes}")
+
+        #find winner
+        if votes > win_votes:
+            win_votes = votes
+            winner = name
+
+           
+    
+    #print winner results
+    print(f"_________________________")
+
+    print(f"Winner: {winner}")
+
+    print(f"_________________________")
+
+
+#output file to Analysis folder
+output_file = os.path.join("Analysis", "Election_Analysis.txt")
+with open(output_file, "w") as results:
+
+    #write analysis report to txt file
+    results.write("Election Results\n")
+    results.write(f"____________________\n")
+    results.write(f"Total Votes: {total_votes}\n") 
+    results.write(f"_________________________\n")
+    results.write(f"{name} : {percent}%, {votes}\n")
+    results.write(f"_________________________\n")
+    results.write(f"Winner: {winner}\n")
+    results.write(f"_________________________\n")
 
 
 
-print(total_votes)
-print(candidate_votes)
 
-#print(candidate_list)
-#print(candidates)
+     
+    
+    
+    
+
+
+        
+
+
